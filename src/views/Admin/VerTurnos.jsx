be3c6dcdc9e22b5a86a6e5fd367de3319c8d8e57
moragentil <moragentil@gmail.com>
2024-12-30 12:06:25 -0300

    estilos turnos

--- src/views/Admin/VerTurnos.jsx
+++ src/views/Admin/VerTurnos.jsx
@@ -65,11 +65,11 @@ function VerTurnos({ selectedDate, onDateChange }) {
         <main className="flex-1 p-6 bg-[#dddcdc]">
           <div className="flex justify-between mb-8">
             <div className="space-y-4">
-              <h1 className="text-2xl font-bold">Turnos</h1>
+              <h1 className="text-2xl font-bold lg:text-4xl">Turnos</h1>
               <div className="flex items-center space-x-2">
-                <span className="text-sm font-medium">Selecciona el Día o Intervalo:</span>
-                <Button onClick={() => setViewOption('day')} variant={viewOption === 'day' ? 'default' : 'outline'} className={`px-4 py-2 ${viewOption === 'day' ? 'bg-naranja text-white' : 'bg-white text-black'}`} style={{ borderRadius: '8px' }}>Día</Button>
-                <Button onClick={() => setViewOption('range')} variant={viewOption === 'range' ? 'default' : 'outline'} className={`px-4 py-2 ${viewOption === 'range' ? 'bg-naranja text-white' : 'bg-white text-black'}`} style={{ borderRadius: '8px' }}>Intervalo</Button>
+                <span className="text-sm font-semibold lg:text-xl">Selecciona el Día o Intervalo:</span>
+                <Button onClick={() => setViewOption('day')} variant={viewOption === 'day' ? 'default' : 'outline'} className={`px-4 py-2 lg:text-lg hover:bg-naranja hover:opacity-70  hover:text-white ${viewOption === 'day' ? 'bg-naranja text-white' : 'bg-white text-naranja border-0'}`} style={{ borderRadius: '8px' }}>Día</Button>
+                <Button onClick={() => setViewOption('range')} variant={viewOption === 'range' ? 'default' : 'outline'} className={`px-4 py-2 lg:text-lg hover:bg-naranja hover:opacity-70 hover:text-white ${viewOption === 'range' ? 'bg-naranja text-white' : 'bg-white text-naranja border-0'}`} style={{ borderRadius: '8px' }}>Intervalo</Button>
               </div>
               <div className='w-full items-center justify-center'>
                 <div className='relative'>
@@ -114,7 +114,7 @@ function VerTurnos({ selectedDate, onDateChange }) {
                   .sort((a, b) => new Date(a) - new Date(b)) // Ordenar las fechas de menor a mayor
                   .map(date => (
                     <div key={date} className='pt-6'>
-                      <h1 className='text-lg font-bold pb-3'>{new Date(date).toLocaleDateString('es-ES', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}</h1>
+                      <h1 className='text-lg font-bold pb-3 lg:text-xl'>{new Date(date).toLocaleDateString('es-ES', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}</h1>
                       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-center items-center">
                         {groupedBookings[date].map((booking) => (
                           <div
@@ -124,18 +124,18 @@ function VerTurnos({ selectedDate, onDateChange }) {
                           >
                             <div className="flex justify-between items-start">
                               <div>
-                                <h3 className="font-semibold">{booking.usuario.nombre}</h3>
-                                <p className="text-sm font-medium text-gray-500">{`${booking.horario.horaInicio} - ${booking.horario.horaFin}`}</p>
-                                <p className='text-sm font-medium text-gray-800'>{`Monto total: $${booking.monto_total}`}</p>
-                                <p className='text-sm font-medium text-gray-800'>{`Monto seña: $${booking.monto_seña}`}</p>
+                                <h3 className="font-semibold lg:text-xl">{booking.usuario.nombre}</h3>
+                                <p className="text-sm font-medium text-gray-500 lg:text-lg">{`${booking.horario.horaInicio} - ${booking.horario.horaFin}`}</p>
+                                <p className='text-sm font-medium text-gray-800 lg:text-lg'>{`Monto total: $${booking.monto_total}`}</p>
+                                <p className='text-sm font-medium text-gray-800 lg:text-lg'>{`Monto seña: $${booking.monto_seña}`}</p>
                               </div>
                             </div>
 
                             <div className="flex flex-wrap gap-2">
-                              <span className="text-center px-3 py-1 bg-gray-300 rounded-full text-xs w-3/4">
+                              <span className="text-center px-3 py-1 bg-gray-300 rounded-full text-xs lg:text-base w-3/4">
                                 {`Estado: ${booking.estado}`}
                               </span>
-                              <span className="text-center px-3 py-1 bg-gray-300 rounded-full text-xs w-3/4">
+                              <span className="text-center px-3 py-1 bg-gray-300 rounded-full lg:text-base text-xs w-3/4">
                                 {`Cancha ${booking.cancha.nro} - ${booking.cancha.tipoCancha}`}
                               </span>
                             </div>
@@ -146,21 +146,21 @@ function VerTurnos({ selectedDate, onDateChange }) {
                                 size="icon"
                                 className="bg-naranja hover:bg-[#FF5533]/90 text-white p-2"
                               >
-                                <Trash2 className="h-4 w-4" />
+                                <Trash2 className="h-4 w-4 lg:h-6 lg:w-6" />
                               </button>
                               <button
                                 style={{ borderRadius: '4px' }}
                                 size="icon"
                                 className="bg-naranja hover:bg-[#FF5533]/90 text-white p-2"
                               >
-                                <PenSquare className="h-4 w-4" />
+                                <PenSquare className="h-4 w-4 lg:h-6 lg:w-6" />
                               </button>
                               <button
                                 style={{ borderRadius: '4px' }}
                                 size="icon"
                                 className="bg-green-500 hover:bg-green-600 text-white p-2"
                               >
-                                <Phone className="h-4 w-4" />
+                                <Phone className="h-4 w-4 lg:h-6 lg:w-6" />
                               </button>
                             </div>
                           </div>

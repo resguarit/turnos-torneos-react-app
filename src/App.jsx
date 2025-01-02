2ec102afde509bfdb9527af5fedb8f3de3ab3a86
moragentil <moragentil@gmail.com>
2025-01-02 10:09:02 -0300

    Nuevo Hero

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
 return (
     <div className="min-h-screen w-screen flex flex-col">
         <Header />
-        <main className=" justify-content-center justify-center items-center flex flex-grow  bg-zinc-500 p-4 font-inter font-semibold tracking-wide text-white">
+        <main className=" flex flex-grow   ">
           <Hero></Hero>
         </main>
         <Footer />

1bee02e6366d1e60bcacabce5e204fcb987c5853
moragentil <moragentil@gmail.com>
2025-01-23 13:55:08 -0300

    falta termianrlo

--- src/components/LoadingSinHF.jsx
+++ src/components/LoadingSinHF.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const LoadingSinHF = () => {
   return (
     <div className=" flex h-[50vh] font-inter">
-      <div className="flex items-center grow bg-gray-100 justify-center w-full space-x-2">
+      <div className="flex items-center grow bg-transparent justify-center w-full space-x-2">
         <div className="w-4 bg-naranja rounded animate-wave"></div>
         <div className="w-4 bg-naranja rounded animate-wave delay-100"></div>
         <div className="w-4 bg-naranja rounded animate-wave delay-200"></div>

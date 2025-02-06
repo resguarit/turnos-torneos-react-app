3da09745d90fc19759f1d9fb797e309e75ab682e
moragentil <moragentil@gmail.com>
2025-02-06 12:51:04 -0300

    pestania canchas respionsive

--- src/views/Admin/PestanaHorario.jsx
+++ src/views/Admin/PestanaHorario.jsx
@@ -342,8 +342,8 @@ const PestanaHorario = () => {
         </div>
 
         {/* Tabla de horarios */}
-        <div className="overflow-x-auto">
-        <table className="min-w-full divide-y divide-gray-200">
+        <div className="overflow-x-auto rounded-[8px]">
+        <table className="min-w-full divide-y divide-gray-200  ">
           <thead className="bg-gray-50">
             <tr>
               <th className="px-4 py-1 sm:px-6 sm:py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-1/4">Día</th>

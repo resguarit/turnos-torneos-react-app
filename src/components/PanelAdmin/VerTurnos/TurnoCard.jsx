355e663e63ca84711f4a38da9a35cceb739cdac2
moragentil <moragentil@gmail.com>
2025-02-14 12:33:58 -0300

    estilos carta turnos

--- src/components/PanelAdmin/VerTurnos/TurnoCard.jsx
+++ src/components/PanelAdmin/VerTurnos/TurnoCard.jsx
@@ -13,7 +13,7 @@ const TurnoCard = ({ booking, handleDeleteSubmit }) => {
     >
       <div className="flex justify-between items-start">
         <div>
-          <h3 className="font-semibold text-xl">{booking.usuario.nombre}</h3>
+          <h3 className="font-semibold text-lg capitalize">{booking.usuario.nombre}</h3>
           <p className="text-sm font-medium text-gray-500">{`${booking.horario.hora_inicio} - ${booking.horario.hora_fin}`}</p>
           <p className="text-sm font-medium text-gray-800">{`Monto total: $${booking.monto_total}`}</p>
           <p className="text-sm font-medium text-gray-800">{`Monto seña: $${booking.monto_seña}`}</p>
@@ -41,28 +41,25 @@ const TurnoCard = ({ booking, handleDeleteSubmit }) => {
 
       <div className="flex gap-2 justify-center">
         <button
-          style={{ borderRadius: '4px' }}
           onClick={() => handleDeleteSubmit(booking)}
           size="icon"
-          className="bg-orange-500 hover:bg-orange-600 text-white p-2 transition-colors duration-200"
+          className="bg-naranja hover:bg-naranja/90 text-white p-2 transition-colors duration-200"
         >
-          <Trash2 className="h-5 w-5" />
+          <Trash2 className="h-4 w-4" />
         </button>
         <button
-          style={{ borderRadius: '4px' }}
           size="icon"
-          className="bg-orange-500 hover:bg-orange-600 text-white p-2 transition-colors duration-200"
+          className="bg-naranja hover:bg-naranja/90 text-white p-2 transition-colors duration-200"
           onClick={() => navigate(`/editar-turno/${booking.id}`)}
         >
-          <PenSquare className="h-5 w-5" />
+          <PenSquare className="h-4 w-4" />
         </button>
         <button
-          style={{ borderRadius: '4px' }}
           onClick={() => window.open(`https://api.whatsapp.com/send?phone=549${booking.usuario.telefono}`, '_blank')}
           size="icon"
           className="bg-green-500 hover:bg-green-600 text-white p-2 transition-colors duration-200"
         >
-          <Phone className="h-5 w-5" />
+          <Phone className="h-4 w-4" />
         </button>
       </div>
     </div>

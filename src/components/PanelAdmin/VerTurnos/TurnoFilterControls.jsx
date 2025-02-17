494801243ed0ddbb08e71319f7976234ef1df5c5
moragentil <moragentil@gmail.com>
2025-02-17 13:52:47 -0300

    estilos ver grilla y ver turnos

--- src/components/PanelAdmin/VerTurnos/TurnoFilterControls.jsx
+++ src/components/PanelAdmin/VerTurnos/TurnoFilterControls.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const FilterControls = ({ selectedCourt, setSelectedCourt, selectedStatus, setSelectedStatus, courts, handleStatusChange }) => (
-  <div className="mt-4 p-4 bg-white rounded-lg shadow-lg">
+  <div className="absolute top-60 right-4 mt-4 p-4 px-10 bg-white rounded-[6px] shadow-lg">
     <div className="flex flex-col lg:flex-row gap-4">
       <div className="flex-1">
         <label className="block text-sm font-bold pb-4 text-gray-700">Seleccionar Cancha</label>
         <select
           value={selectedCourt}
           onChange={(e) => setSelectedCourt(e.target.value)}
-          className="w-full pl-3 pr-10 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="w-full  text-sm p-1 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
         >
           <option value="">Todas las canchas</option>
           {courts.map(court => (
@@ -17,8 +17,8 @@ const FilterControls = ({ selectedCourt, setSelectedCourt, selectedStatus, setSe
         </select>
       </div>
       <div className="flex-1">
-        <label className="block text-sm font-bold pb-6 text-gray-700">Seleccionar Estado</label>
-        <div className="flex flex-wrap gap-2">
+        <label className="block text-sm font-bold pb-4 text-gray-700">Seleccionar Estado</label>
+        <div className="grid grid-cols-2  gap-2">
           {['Pendiente', 'Señado', 'Pagado', 'Cancelado'].map(status => (
             <div key={status} className="flex items-center">
               <input

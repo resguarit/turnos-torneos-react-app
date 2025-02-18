97fe8f8cf6d5fc10b8a714a92ae079b1bbdb251f
moragentil <moragentil@gmail.com>
2025-02-18 09:24:20 -0300

    responisve pagina error

--- src/components/ModalConfirmation.jsx
+++ src/components/ModalConfirmation.jsx
@@ -6,10 +6,10 @@ function ModalConfirmation({ onConfirm, onCancel, title, subtitle, botonText1, b
             <div className="z-50 fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center font-inter">
               <div className="bg-white text-black z-20 p-4 rounded-xl shadow-lg w-11/12 md:w-2/3 lg:w-1/3">
                 <div className="flex justify-between">
-                  <h2 className="text-lg font-bold mb-4 md:text-2xl">{title}</h2>
+                  <h2 className="text-lg font-bold mb-4 md:text-xl">{title}</h2>
                   <X onClick={onCancel} className="cursor-pointer" />
                 </div>
-                <p className="mb-6 text-base md:text-base text-gray-600">{subtitle}</p>
+                <p className="mb-6 text-sm md:text-base text-gray-600">{subtitle}</p>
                 <div className="flex justify-evenly">
                     <button
                         onClick={onCancel}

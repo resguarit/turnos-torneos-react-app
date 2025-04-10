d7ec118b1c9237920c89b52c2e076e925db54771
Mariano <marianosalas24@gmail.com>
2025-04-10 10:40:51 -0300

    Actualización de la configuración de Vite para cambiar la base de la aplicación a la raíz. Esto permite una mejor gestión de las rutas en el entorno de desarrollo.

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/preview/frontturnos.com.ar',
+  base: '/',
   build: {
     assetsDir: 'assets',
     emptyOutDir: true,

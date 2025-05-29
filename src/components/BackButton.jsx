d05b60eecc7c78a83f4ff0eb51bf8cf11c734f76
moragentil <moragentil@gmail.com>
2025-05-29 11:10:01 -0300

    backbutton nuevo

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -1,16 +1,13 @@
 import { useNavigate } from "react-router-dom";
-import { ArrowLeft } from "lucide-react"; // Icono de flecha izquierda
+import { ChevronLeft } from "lucide-react"; // Icono de flecha izquierda
 
-function BackButton() {
-    const navigate = useNavigate(); // Hook para la navegación
+function BackButton({ruta}) {
+    const navigate = useNavigate(); 
 
     return (
-        <button
-            onClick={() => navigate(-1)} // Navegar hacia la pantalla anterior
-            className="bg-naranja flex justify-center font-semibold text-white hover:bg-black/90 lg:text-xl w-14 py-1 mb-4 border border-white rounded-[10px]"
-        >
-           <ArrowLeft />
-        </button>
+        <button onClick={() => navigate(`${ruta}`)} className="bg-black rounded-xl text-white p-2 text-sm flex items-center justify-center">
+            <ChevronLeft className="w-5" /> Atrás
+          </button>
     );
 }
 

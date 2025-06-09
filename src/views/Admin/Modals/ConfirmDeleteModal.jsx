aa819ad2d256a0f331fd16bdee3a3854128a9cee
moragentil <moragentil@gmail.com>
2025-06-09 11:29:41 -0300

    mensaje de advertencia opcional

--- src/views/Admin/Modals/ConfirmDeleteModal.jsx
+++ src/views/Admin/Modals/ConfirmDeleteModal.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function ConfirmDeleteModal({ isOpen, onClose, onConfirm, loading, accionTitulo, accion, pronombre, entidad, accionando, nombreElemento }) {
+export default function ConfirmDeleteModal({ isOpen, onClose, onConfirm, loading, accionTitulo, accion, pronombre, entidad, accionando, nombreElemento, advertencia }) {
   if (!isOpen) return null;
 
   return (
@@ -11,8 +11,13 @@ export default function ConfirmDeleteModal({ isOpen, onClose, onConfirm, loading
         {nombreElemento !== undefined && (
          <div className="font-semibold mb-2 text-center text-gray-900 bg-gray-50 px-3 py-2 rounded-[8px] border">"{nombreElemento}"</div>
          )}
-        <p className='mb-6 text-red-600'>Esta acción no se puede deshacer.</p>
-        <div className="flex justify-end gap-3">
+        <p className=' text-red-600'>Esta acción no se puede deshacer.</p>
+         {advertencia !== undefined && ( 
+          <div className="mt-2 w-full bg-red-100 border-l-red-500 border-l-2 p-2 rounded-md mb-4 flex items-center">
+            <p className="text-sm text-red-700">{advertencia}</p>
+          </div>
+         )} 
+        <div className="flex mt-6 justify-end gap-3">
           <button
             onClick={onClose}
             className="px-3 py-2 bg-gray-200 text-gray-800 rounded hover:bg-gray-300"

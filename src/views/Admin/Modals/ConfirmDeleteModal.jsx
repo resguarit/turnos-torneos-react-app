ba226b151869f595187739f1bb4f8fac7a3b052e
moragentil <moragentil@gmail.com>
2025-06-09 09:16:41 -0300

    nombre de elemento a eliminar en el modal de confrimar eliminacion

--- src/views/Admin/Modals/ConfirmDeleteModal.jsx
+++ src/views/Admin/Modals/ConfirmDeleteModal.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
-export default function ConfirmDeleteModal({ isOpen, onClose, onConfirm, loading, accionTitulo, accion, pronombre, entidad, accionando }) {
+export default function ConfirmDeleteModal({ isOpen, onClose, onConfirm, loading, accionTitulo, accion, pronombre, entidad, accionando, nombreElemento }) {
   if (!isOpen) return null;
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
       <div className="bg-white p-6 rounded-[8px] shadow-lg w-[90%] max-w-md">
         <h2 className="text-lg font-bold text-gray-800 mb-4">Confirmar {accionTitulo}</h2>
-        <p className="text-gray-600 mb-6">¿Estás seguro de que deseas {accion} {pronombre} {entidad}? Esta acción no se puede deshacer.</p>
+        <p className="text-gray-600 mb-2 ">¿Estás seguro de que deseas {accion} {pronombre} {entidad}{nombreElemento !== undefined ? ':' : '?'}</p>
+        {nombreElemento !== undefined && (
+         <div className="font-semibold mb-2 text-center text-gray-900 bg-gray-50 px-3 py-2 rounded-[8px] border">"{nombreElemento}"</div>
+         )}
+        <p className='mb-6 text-red-600'>Esta acción no se puede deshacer.</p>
         <div className="flex justify-end gap-3">
           <button
             onClick={onClose}

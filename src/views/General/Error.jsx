20c4117c5074075559f90dd3197e49a4eb5b667d
Mariano <marianosalas24@gmail.com>
2025-06-11 20:15:22 -0300

    fix Error.jsx

--- src/views/General/Error.jsx
+++ src/views/General/Error.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Header } from '../../components/header';
-import { Footer } from '../../components/footer';
-import { Button } from '../../components/ui/button';
+import { Header } from '@/components/Header';
+import { Footer } from '@/components/Footer';
+import { Button } from '@/components/ui/button';
 
 function Error() {
   return (

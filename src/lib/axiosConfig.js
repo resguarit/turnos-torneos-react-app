4981a59c3b9f1cd7fee10c660492f0154596214e
Mariano <marianosalas24@gmail.com>
2025-06-26 08:52:04 -0300

    feat: Añadir workflow de despliegue para frontend Rock & Gol

--- src/lib/axiosConfig.js
+++ src/lib/axiosConfig.js
@@ -8,11 +8,9 @@ const subdomain = parts.length >= 2 ? parts[0] : null;
 console.log(subdomain);
 
 const api = axios.create({
-  //baseURL: 'https://rockandgolback-production.up.railway.app/api',
   //baseURL: 'http://localhost:8000/api',
-  //baseURL: 'https://vps-4793092-x.dattaweb.com:8090/preview/turnos.com.ar/api',
-  baseURL: 'https://api.rgturnos.com.ar/api',
-  //baseURL: 'https://7c22-191-84-231-79.ngrok-free.app/api'
+  //baseURL: 'https://api.rgturnos.com.ar/api',
+  baseURL: 'https://api.complejorockandgol.com.ar/api',
 });
 
 // Interceptor para agregar el token a todas las solicitudes
